0b345f2138f49a9ac05f1a0e94f0069ec2ce048e
lmpmrsx <landry.molinier@gmail.com>
2016-04-24 16:25:07 +0200

    commit_v1.1

--- Pjt_Corba_M2/src/LanceurEntiteLogicielle/EntiteLogicielleAutorisation.java
+++ Pjt_Corba_M2/src/LanceurEntiteLogicielle/EntiteLogicielleAutorisation.java
@@ -1,10 +1,49 @@
 package LanceurEntiteLogicielle;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.omg.CosNaming.*;
+import org.omg.PortableServer.*;
+
+import impl.EntiteLogicielleAutorisationImpl;
+
 public class EntiteLogicielleAutorisation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			// Intialisation de l'ORB
+	        //************************
+	        org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args,null);
 
+	        // Gestion du POA
+	        //****************
+	        // Recuperation du POA
+	        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+
+	        // Creation du servant
+	        //*********************
+	        EntiteLogicielleAutorisationImpl monAutorisation = new EntiteLogicielleAutorisationImpl();
+
+	        // Activer le servant au sein du POA et recuperer son ID
+	        byte[] monEuroId = rootPOA.activate_object(monAutorisation);
+
+	        // Activer le POA manager
+	        rootPOA.the_POAManager().activate();
+	        
+	        String IORServant = orb.object_to_string(rootPOA.servant_to_reference(monAutorisation));
+	        System.out.println("L'objet possede la reference suivante :");
+	        System.out.println(IORServant);
+
+	        // Lancement de l'ORB et mise en attente de requete
+	        //**************************************************
+	        orb.run();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
---
+++ Pjt_Corba_M2/src/impl/EntiteLogicielleAutorisationImpl.java
@@ -0,0 +1,54 @@
+package impl;
+
+import modControledAcces.CleInconnue;
+import modControledAcces.Date;
+import modControledAcces.EntiteLogicielleAutorisationPOA;
+import modControledAcces.EntiteLogicielleEmpreinte;
+import modControledAcces.EntiteLogiciellePorte;
+import modControledAcces.ErreurSalarieInexistant;
+import modControledAcces.ErreurZoneInexistant;
+import modControledAcces.InfoSalarieAccreditation;
+
+public class EntiteLogicielleAutorisationImpl extends EntiteLogicielleAutorisationPOA {
+
+	@Override
+	public String[] listeToutesZones() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean verifierAutorisation(String idSal, String idZone, EntiteLogiciellePorte el_Porte)
+			throws ErreurSalarieInexistant, ErreurZoneInexistant {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void creerAccreditation(String idSal, String idZone, Date dateAccessDebut, Date dateAccessFin,
+			String cleDemandeur) throws ErreurSalarieInexistant, CleInconnue, ErreurZoneInexistant {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public InfoSalarieAccreditation lireAccreditationSalarie(String idSal) throws ErreurSalarieInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InfoSalarieAccreditation modifierAccreditation(String idSal, String idZone,
+			EntiteLogicielleEmpreinte el_Empreinte) throws ErreurSalarieInexistant, ErreurZoneInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InfoSalarieAccreditation supprimerAccreditation(String idSal, String idZone,
+			EntiteLogicielleEmpreinte el_Empreinte) throws ErreurSalarieInexistant, ErreurZoneInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
