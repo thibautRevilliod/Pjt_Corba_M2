354ea296ed9b39cce2c6ec05bbe44a1955d4a876
Thibaut Revilliod <thibaut.revilliod@gmail.com>
2016-04-30 17:23:03 +0200

    journalisation

---
+++ Pjt_Corba_M2/src/impl/EntiteLogicielleJournalisationImpl.java
@@ -0,0 +1,50 @@
+package impl;
+
+import modControledAcces.Date;
+import modControledAcces.EntiteLogicielleAnnuairePOA;
+import modControledAcces.EntiteLogicielleEmpreinte;
+import modControledAcces.EntiteLogiciellePorte;
+import modControledAcces.ErreurSalarieExistant;
+import modControledAcces.ErreurSalarieInexistant;
+import modControledAcces.InfoSalarie;
+
+public class EntiteLogicielleJournalisationImpl extends EntiteLogicielleAnnuairePOA {
+	
+	@Override
+	public InfoSalarie[] listeTousSalaries() {
+		
+		InfoSalarie infoSalarie = new InfoSalarie();
+		InfoSalarie[] listeSalaries = {infoSalarie};
+		
+		System.out.println("test !!!!!");
+		return listeSalaries;
+	}
+
+	@Override
+	public InfoSalarie recupererNomPhotoSalarie(String idSal, EntiteLogicielleEmpreinte el_Empreinte)
+			throws ErreurSalarieInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InfoSalarie infosSalarie(String idSal, EntiteLogicielleEmpreinte el_Empreinte)
+			throws ErreurSalarieInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String creerSalarie(String mdp, String nom, String prenom, String photo, Date heureDebut, Date heureFin,
+			Date jourDebut, Date jourFin, short dureeValidationCompte) throws ErreurSalarieExistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String sauthentifier(String photo, EntiteLogiciellePorte el_Porte) throws ErreurSalarieInexistant {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
