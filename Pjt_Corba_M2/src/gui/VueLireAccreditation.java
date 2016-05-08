0cb27bfaade74b989fd3833efa44481005ee5a4b
melanievivies <ml.31@hotmail.fr>
2016-05-08 10:48:41 +0200

    Lire accreditation et supprimer accreditation -> modifications de l'interface

--- Pjt_Corba_M2/src/gui/VueLireAccreditation.java
+++ Pjt_Corba_M2/src/gui/VueLireAccreditation.java
@@ -8,11 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
 
 public class VueLireAccreditation extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JLabel lblIdentifiantDeLa;
+	private JComboBox comboBox;
 
 	/**
 	 * Launch the application.
@@ -43,12 +47,20 @@ public class VueLireAccreditation extends JFrame {
 		contentPane.setLayout(null);
 		
 		table = new JTable();
-		table.setBounds(35, 40, 353, 175);
+		table.setBounds(35, 82, 353, 133);
 		contentPane.add(table);
 		
 		JButton btnQuitter = new JButton("Quitter");
 		btnQuitter.setBounds(152, 227, 89, 23);
 		contentPane.add(btnQuitter);
+		
+		lblIdentifiantDeLa = new JLabel("Identifiant de la personne \u00E0 consulter : ");
+		lblIdentifiantDeLa.setBounds(25, 32, 249, 14);
+		contentPane.add(lblIdentifiantDeLa);
+		
+		comboBox = new JComboBox();
+		comboBox.setBounds(311, 29, 28, 20);
+		contentPane.add(comboBox);
 		
 		
 		// abonnements
--- Pjt_Corba_M2/src/gui/VueSupprimerAccreditation.java
+++ Pjt_Corba_M2/src/gui/VueSupprimerAccreditation.java
@@ -37,27 +37,35 @@ public class VueSupprimerAccreditation extends JFrame {
 	public VueSupprimerAccreditation() {
 		setTitle("Supprimer une accreditation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 392, 145);
+		setBounds(100, 100, 392, 198);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblIdentifiantDeLaccreditation = new JLabel("Identifiant de l'accreditation \u00E0 supprimer : ");
+		JLabel lblIdentifiantDeLaccreditation = new JLabel("Identifiant de la personne : ");
 		lblIdentifiantDeLaccreditation.setBounds(10, 23, 250, 14);
 		contentPane.add(lblIdentifiantDeLaccreditation);
 		
 		JButton btnValider = new JButton("Valider ");
-		btnValider.setBounds(77, 73, 89, 23);
+		btnValider.setBounds(80, 125, 89, 23);
 		contentPane.add(btnValider);
 		
 		JButton btnQuitter = new JButton("Quitter");
-		btnQuitter.setBounds(190, 73, 89, 23);
+		btnQuitter.setBounds(187, 125, 89, 23);
 		contentPane.add(btnQuitter);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(248, 20, 28, 20);
 		contentPane.add(comboBox);
+		
+		JLabel lblIdentifiantDeLa = new JLabel("Identifiant de la zone : ");
+		lblIdentifiantDeLa.setBounds(10, 59, 139, 14);
+		contentPane.add(lblIdentifiantDeLa);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(248, 59, 28, 20);
+		contentPane.add(comboBox_1);
 		
 		
 		// abonnement
