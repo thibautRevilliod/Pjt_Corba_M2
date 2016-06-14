966ad96a34ba6a69fb5b9d96b7d15cc499ff6e84
lmpmrsx <landry.molinier@gmail.com>
2016-06-14 18:33:50 +0200

    estSoutenanceDistante

--- Pjt_Corba_M2/src/config/Config.java
+++ Pjt_Corba_M2/src/config/Config.java
@@ -2,10 +2,6 @@ package config;
 
 public class Config {
 	
-	public static String IOR_EL_Annuaire;
-	public static String IOR_EL_Autorisation;
-	public static String IOR_EL_Empreinte;
-	public static String IOR_EL_Journalisation;
-	public static String IOR_EL_Porte;
+	public static boolean estSoutenanceDistante = true;
 
 }
