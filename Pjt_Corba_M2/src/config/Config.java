9d55dc9b98415b3399d3acca6a4f2e30d515a569
lmpmrsx <landry.molinier@gmail.com>
2016-06-14 18:39:13 +0200

    config

--- Pjt_Corba_M2/src/config/Config.java
+++ Pjt_Corba_M2/src/config/Config.java
@@ -2,6 +2,11 @@ package config;
 
 public class Config {
 	
+	public static String IOR_EL_Annuaire;
+	public static String IOR_EL_Autorisation;
+	public static String IOR_EL_Empreinte;
+	public static String IOR_EL_Journalisation;
+	public static String IOR_EL_Porte;
 	public static final boolean estSoutenanceDistante = true;
 	public static final String ipServeur = "169.254.77.228";
 	public static final String ipClient = "169.254.77.228";
